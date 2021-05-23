315e49feaeffc1b1bafcb70a571ccbb3ca75a8cd
gurnhdo <pixidao154kgz@126.com>
2021-05-23 17:09:31 +0800

    基本配置和mysql配置

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+class config():
+    DEBUG =True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/info16"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+
+app.config.from_object(config)
+db = SQLAlchemy(app)
+
+
+
 
 
 @app.route('/')
