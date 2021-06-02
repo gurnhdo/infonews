202b444cd3a654b56ba271d137b6bc5275b7d58b
gurnhdo <pixidao154kgz@126.com>
2021-06-02 22:33:03 +0800

    抽取配置模块化

---
+++ config.py
@@ -0,0 +1,24 @@
+from datetime import timedelta
+
+from redis import StrictRedis
+
+
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/info16"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    SECRET_KEY = "/A9rbIovhq6h7mFG4O59HXf1M+qFJxBvzCn4fPhZfpSHXuASEtzcj64Tm7LbQ5E3"
+    PERMANENT_SESSION_LIFETIME = timedelta(days=7)
+
+
+class DevelopConfig(Config):
+    DEBUG = True
+
+class ProduceConfig(Config):
+    DEBUG = False
