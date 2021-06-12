3d21dc67d067d65da61cb4b4ff5f0c90b3223541
gurnhdo <pixidao154kgz@126.com>
2021-06-12 17:55:16 +0800

    数据库变量定义为全局变量

--- info/__init__.py
+++ info/__init__.py
@@ -3,19 +3,19 @@ from flask_migrate import Migrate
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
-
 from config import congfig_dict
+
+sr = None #type:StrictRedis
+db = None #type:SQLAlchemy
+
 # 定义一个函数 让代码作为功能使用 不主动执行 封装起来让别的程序调用
 # 工厂函数
-from info.modules.home import home_blu
-
-
 def create_app(config_type):
     config_class=congfig_dict[config_type]
 
 
     app = Flask(__name__)
-
+    global sr,db
     app.config.from_object(config_class)
     db = SQLAlchemy(app)
     sr = StrictRedis(host=config_class.REDIS_HOST, port=config_class.REDIS_PORT)
@@ -23,6 +23,7 @@ def create_app(config_type):
     # 初始化迁移器
     Migrate(app, db)
 
+    from info.modules.home import home_blu
     app.register_blueprint(home_blu)
 
     return app
--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -1,3 +1,4 @@
+from info import sr
 from .import home_blu
 
 
@@ -5,4 +6,5 @@ from .import home_blu
 def index():
     # sr.set("name", "zs")
     # session["name"] = "ls"
+    sr.set("name","zs")
     return 'index11111'
